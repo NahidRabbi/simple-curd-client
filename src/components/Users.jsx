89daa2b81f913835d7735c4ff69bac1fbecc976a
nahid <nahidrabbi045@gmail.com>
2024-05-06 17:30:18 +0600

    added update section

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,10 @@
-import { useLoaderData } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Users = () => {
 
-   const users = useLoaderData();
+   const loadedUsers = useLoaderData();
+   const [users,setUsers] = useState(loadedUsers);
 
    const handleDelete = _id =>{
       console.log('delete',_id)
@@ -14,6 +16,8 @@ const Users = () => {
          console.log(data);
          if(data.deletedCount>0){
             alert('deleted successfully');
+            const remaining = users.filter(user =>user._id !== _id);
+            setUsers(remaining);
          }
       })
    }
@@ -23,8 +27,13 @@ const Users = () => {
             <h2>{users.length}</h2>
             <div>
                 {
-                     users.map(user => <p key={user._id} >{user.name} : {user.email} {user._id}<button 
-                       onClick ={ () => handleDelete(user._id)}
+                     users.map(user => <p key={user._id} >
+                           {user.name} : {user.email} {user._id} 
+                           <Link to={`/update/${user._id}`}>
+                           <button>Update</button>
+                           </Link>
+                        <button 
+                           onClick ={ () => handleDelete(user._id)}
                      >x</button></p>)
                 }
             </div>
